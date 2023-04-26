89e97281243f25ed16a1d422373aeec67e647125
elinsauer1 <elinsauer@hotmail.com>
2023-04-26 15:07:22 +0200

    RenderProfileView

--- js/components.js
+++ js/components.js
@@ -29,11 +29,38 @@ function renderLoggedInView(activeTab) {
 renderLoggedInView("myProfile");
 
 
-function renderUserPage(userInfo) {
+function renderProfileView(userInfo) {
 
+    const profile_picture = userInfo.userIdentity.profilePic;
+    const user_followers = userInfo.userSocial.following.length;
+    const user_following = userInfo.userSocial.followers.length;
+    const username = userInfo.userCredentials.userName;
 
-    const html = document.querySelector("body").innerHTML = `
-    
+    const html = document.querySelector("#main_block").innerHTML = `
+    <header id="profile_header">
+        <div>
+            <div id="profile_picture">
+                <img src="${profile_picture}">
+            </div>
+            <p>@${username}</p>
+        </div> 
+        <div>
+            <div id="following_followers">
+                <div>Followers</div>
+                <div>${user_followers}</div>
+                <div>Following</div>
+                <div>${user_following}</div>
+            </div>
+            <div>
+                <div id="settings_icon"></div>
+                <div id="saved_albums_icon"></div>
+                <div id="add_board_icon"></div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <h1>BOARDS</h1>
+        <div id="Board_of_boards"></div>
+    </main>
     `
-
 }
