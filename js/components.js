bb7056aea7f6461f62b435427b30e24109392b38
TheaGenrup <theagenrup@gmail.com>
2023-04-26 11:53:05 +0200

    js

---
+++ js/components.js
@@ -0,0 +1,40 @@
+function renderLoggedInView(activeTab) {
+    console.log(activeTab.toLowerCase());
+
+    document.querySelector("body").innerHTML = `
+    <header>
+        <div id="laulu">LAULU</div>
+        <div id="search_icon"></div>
+    </header>
+    <main>
+        <div id="tabs">
+            <div class="tab">DISCOVER</div>
+            <div class="tab">MY PROFILE</div>
+        </div>
+        <div id="main_block"></div>
+    </main>
+    `
+
+
+    document.querySelectorAll(".tab").forEach(tab => {
+        console.log(tab.textContent.toLowerCase().replace(" ", ""));
+        if (tab.textContent.toLowerCase().replace(" ", "") == activeTab.toLowerCase()) {
+            tab.classList.add("active")
+        } else {
+            tab.classList.add("inactive")
+        }
+    });
+
+}
+
+renderLoggedInView("myProfile");
+
+
+function renderUserPage(userInfo) {
+
+
+    const html = document.querySelector("body").innerHTML = `
+    
+    `
+
+}
