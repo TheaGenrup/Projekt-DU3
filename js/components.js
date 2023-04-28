0e56ffdb6e783fa8f35bb49cdf15bff096738b12
TheaGenrup <theagenrup@gmail.com>
2023-04-28 11:15:23 +0200

    allt funkar nu även med flera reviews

--- js/components.js
+++ js/components.js
@@ -2,9 +2,6 @@ function renderLoggedInView(activeTab) {
 
     document.querySelector("body").innerHTML = `
 
-    <header>
-        <div id="laulu">LAULU</div>
-    </header>
     <main>
     </main>
     <nav>
@@ -32,10 +29,10 @@ renderLoggedInView("myProfile");
 
 function renderDiscoverView(reviews) {
 
+    document.querySelector("main").innerHTML = `<div id="content_container"></div>`;
+
     // go through all reviews to create them
     reviews.forEach(review => {
-
-        document.querySelector("main").innerHTML = `<div id="content_container"></div>`;
 
         // shorten comment
         let comment = review.comment;
@@ -50,12 +47,12 @@ function renderDiscoverView(reviews) {
             <p id="who">@ ${review.user_name} added a review</p>
             <p id="when">${review.date} ${review.timestamp}</p>
             <div id="album_overview">
-                <div id="album_cover"></div>
+                <div id="album_cover_${review.review_id}" class="album_cover"></div>
                     <div id="album_details">
 
                         <p id="album_name">${review.album_name}</p>
                         <p id="artist_name">${review.artist_name}</p>
-                        <div id="stars">
+                        <div id="stars_${review.review_id}" class="stars">
                             <div class="star"></div>
                             <div class="star"></div>
                             <div class="star"></div>
@@ -71,10 +68,10 @@ function renderDiscoverView(reviews) {
         document.querySelector("#content_container").innerHTML += newReview;
 
         // add album cover
-        document.querySelector("#album_cover").style.backgroundImage = `url(../media/dreams.jpg)`;
+        document.querySelector(`#album_cover_${review.review_id}`).style.backgroundImage = review.album_cover;
 
         // change the background image of the right amount of stars
-        const stars = document.querySelectorAll(".star");
+        const stars = document.querySelectorAll(`#stars_${review.review_id} > div`);
 
         for (let i = 0; i < stars.length; i++) {
             const star = stars[i];
@@ -83,8 +80,6 @@ function renderDiscoverView(reviews) {
             }
         }
 
-
-
     });
 
 
@@ -92,6 +87,7 @@ function renderDiscoverView(reviews) {
 
 renderDiscoverView([
     {
+        review_id: 1,
         album_name: "Dreams",
         artist_name: "Fleetwood Mac",
         user_name: "Elin",
@@ -99,5 +95,17 @@ renderDiscoverView([
         timestamp: "16:35",
         comment: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad unde ea laborum tempora quidem sint commodi culpa dolorum fugiat illum.",
         rating: 3,
+        album_cover: `url(../media/dreams.jpg)`
+    },
+    {
+        review_id: 2,
+        album_name: "Och Stora Havet",
+        artist_name: "Jakob Hellman",
+        user_name: "Thea",
+        date: "2023-02-25",
+        timestamp: "14:38",
+        comment: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad unde ea laborum tempora quidem sint commodi culpa dolorum fugiat illum.",
+        rating: 4,
+        album_cover: `url(../media/hellman.jpg)`
     }
 ]);
