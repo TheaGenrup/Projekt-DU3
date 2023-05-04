a4cc5574cef04a277be0c1abf122ce62514accec
RosFilip <filipGithub@gmail.com>
2023-05-04 17:32:38 +0200

    HotFix: CSS
    Fixade bara så att css inte strulade, ska nu påbörja söka efter användare

--- js/search.js
+++ js/search.js
@@ -46,70 +46,81 @@ function closeSearchWindow() {
     searchFieldContainer.style.width = "0%";
     searchFieldContainer.style.height = "0%";
     searchField.style.display = "none";
+    // Kanske ta bort? Ska vi rensa lista efter man stängt ner den?
+    const albumUl = document.querySelector("#albumUl");
+    albumUl.innerHTML = "";
 }
 
 function searchAlbums(e) {
     let input = e.target.value;
-    if (input != "" || input === null || input === undefined) {
-      let albumSearchEndpoint = `https://api.spotify.com/v1/search?q=${input}&type=album&offset=0&limit=10`;
-      
-  
-      if (token.length > 0 || token != undefined) { 
-        fetch(albumSearchEndpoint, {
-            headers: {
-                "Authorization": "Bearer " + token
-            }
-        })
-        .then(response => response.json())
-        .then(albumResource => {
-          const albumsfetched = albumResource.albums.items;
-          const albumsFound = []
-          albumsfetched.forEach(album => {
-            const artistsInAlbum = []; 
-            album.artists.forEach(artist => {
-              artistsInAlbum.push(artist.name)
-            });
-            let albumInfo = {
-              artistName: artistsInAlbum,
-              albumName:  album.name,
-              albumId:    album.id,
-              albumImage: album.images[1].url,
-              albumType:  album.album_type,
-            }
-            albumsFound.push(albumInfo)
+    if (input === "" || input === null || input === undefined) {return}
+    let albumSearchEndpoint = `https://api.spotify.com/v1/search?q=${input}&type=album&offset=0&limit=10`;
+
+    if (token.length > 0 || token != undefined) { 
+      fetch(albumSearchEndpoint, {
+          headers: {
+              "Authorization": "Bearer " + token
+          }
+      })
+      .then(response => response.json())
+      .then(albumResource => {
+        const albumsfetched = albumResource.albums.items;
+        const albumsFound = []
+        albumsfetched.forEach(album => {
+          const artistsInAlbum = []; 
+          album.artists.forEach(artist => {
+            artistsInAlbum.push(artist.name)
           });
-          listAlbums(albumsFound);
+          let albumInfo = {
+            artistName: artistsInAlbum,
+            albumName:  album.name,
+            albumId:    album.id,
+            albumImage: album.images[1].url,
+            albumType:  album.album_type,
+          }
+          albumsFound.push(albumInfo)
         });
-  
-      }
+        listAlbums(albumsFound);
+      });
+
     }
+}
+function searchUsers(e) {
+    let input = e.target.value;
+    if (input === "" || input === null || input === undefined) {return}
+    const request = new Request(`/server/searchUsers.php?search=${input}`);
+    fetch(request)
+        .then(r=>r.json())
+        .then(userResource => {
+            console.log(userResource);
+        })
 }
 
 function listAlbums(albums) {
     const albumUl = document.querySelector("#albumUl");
     albumUl.innerHTML = "";
     if (albums.length > 0) {
-        console.log("TJOHO");
+        albums.forEach(album => {
+            const artistName = album.artistName;
+            const albumName = album.albumName;
+            const albumId = album.albumId;
+            const albumImage = album.albumImage;
+            const albumType = album.albumType;
+    
+            const html = `
+                <img class ="albumPreviewImage" src="${albumImage}" alt="${albumName}">
+                <div class="albumInfo">
+                    <p>${artistName}</p>
+                    <p>${albumName}</p>
+                    <p>${albumType}</p>
+                </div>
+                <img class="saveToListBtn" src="./media/icons/Add.png" alt="Press to save album">
+            `;
+            const liDom = document.createElement("li");
+            liDom.setAttribute("id", albumId);
+            liDom.classList.add("albumPreview");
+            liDom.innerHTML = html;
+            albumUl.append(liDom);
+        });
     }
-    albums.forEach(album => {
-        const artistName = album.artistName;
-        const albumName = album.albumName;
-        const albumId = album.albumId;
-        const albumImage = album.albumImage;
-        const albumType = album.albumType;
-
-        const html = `
-            <img class ="albumPreviewImage" src="${albumImage}" alt="${albumName}">
-            <div class="albumInfo">
-                <p>Artist</p>
-                <p>${albumName}</p>
-                <p>${albumType}</p>
-            </div>
-        `;
-        const liDom = document.createElement("li");
-        liDom.setAttribute("id", albumId);
-        liDom.classList.add("albumPreview");
-        liDom.innerHTML = html;
-        albumUl.append(liDom);
-    });
 }
